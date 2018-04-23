f721a4d54b8a955cc17f827c9c888fd24295ecb5
Martin Kimmig <martin.kimmig@students.fhnw.ch>
2018-04-23 15:37:56 +0200

    Initial display implementation of placement versus marketvalue data

--- src/footballBubbleChart.js
+++ src/footballBubbleChart.js
@@ -23,4 +23,95 @@ svg.append("text")
     .attr("font-family", "sans-serif")
     .attr("font-size", "24px")
     .style("text-anchor", "left")
-    .text("Football-Which team is really efficient?");
+    .text("Football - Which team is really efficient?");
+
+
+// load the data from the cleaned csv file.
+// note: the call is done asynchronous.
+// That is why you have to load the data inside of a
+// callback function.
+d3.csv("./data/bundesligaDaten3.csv", function(error, data) {
+    //const valueDomain = d3.extent(data, d => Number(d.Gesamtmarktwert));
+    //const successDomain = d3.extent(data, d => Number(d.Platzierung));
+
+    const valueDomain = [d3.max(data, d => Number(d.Gesamtmarktwert)),d3.min(data, d => Number(d.Gesamtmarktwert))];
+    const successDomain = [d3.max(data, d => Number(d.Platzierung)),d3.min(data, d => Number(d.Platzierung))];
+
+    console.log(data);
+    // create scales for x and y direction
+    const xScale = d3.scaleLinear()
+        .range([0,width])
+        .domain(valueDomain);
+        //.nice(5);
+
+    const yScale = d3.scaleLinear()
+        .range([height,0])
+        .domain(successDomain);
+        //.nice(1);
+
+    console.log(valueDomain);
+    console.log(successDomain);
+
+    //const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+    // create xAxis
+    const xAxis = d3.axisBottom(xScale);
+    g.append("g")  // create a group and add axis
+        .attr("transform", "translate(0," + height + ")").call(xAxis);
+
+    // create yAxis
+    const yAxis = d3.axisLeft(yScale);
+    g.append("g")  // create a group and add axis
+        .call(yAxis);
+
+
+    // add circle
+    var data_points = g.selectAll("circle")  // this is just an empty placeholder
+        .data(data)
+        .enter().append("circle")
+        .attr("class", "bar")
+        .attr("cx", d => xScale(d.Gesamtmarktwert))
+        .attr("cy", d => yScale(d.Platzierung))
+        .attr("r", 4)
+        .style("fill", "black");
+
+
+    // Create tooltip
+    var tooltip = d3.select("body").append("div").classed("tooltip", true);
+
+    data_points.on("mouseover", function(d, i) {
+        tooltip
+            .html(`${d["Verein"]} <br/>`
+                + `Gesamtmarktwert: ${d.Gesamtmarktwert}<br/>`
+                + `Platzierung: ${d.Platzierung}<br/>`
+                 )
+            .style("visibility", "visible")
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+    })
+        .on("mouseout", function(d,i) {
+            tooltip.style("visibility", "hidden")
+        });
+
+
+
+});
+
+// text label for the x axis
+g.append("text")
+    .attr("y", height + margin.bottom / 2)
+    .attr("x", width / 2)
+    .attr("dy", "1em")
+    .attr("font-family", "sans-serif")
+    .style("text-anchor", "middle")
+    .text("GesamtMarktwert in Mio €");
+
+// text label for the y axis
+g.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x",0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("font-family", "sans-serif")
+    .style("text-anchor", "middle")
+    .text("Platzierung");
